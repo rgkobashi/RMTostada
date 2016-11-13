15c92ed717c164929979d149a47fc36044126b74
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2016-11-13 02:42:51 -0500

    RMTostadaView created

--- RMTostada/RMTostadaView.swift
+++ RMTostada/RMTostadaView.swift
@@ -10,5 +10,26 @@ import UIKit
 
 class RMTostadaView: UIView
 {
-
+    var fadeDuration: NSTimeInterval!
+    var shiftDuration: NSTimeInterval!
+    var verticalPadding: CGFloat!
+    
+    init(frame: CGRect, fadeDuration: NSTimeInterval, shiftDuration: NSTimeInterval, verticalPadding: CGFloat)
+    {
+        super.init(frame: frame)
+        self.fadeDuration = fadeDuration
+        self.shiftDuration = shiftDuration
+        self.verticalPadding = verticalPadding
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func identifier() -> String
+    {
+        return String(ObjectIdentifier(self).uintValue)
+    }
 }
+
