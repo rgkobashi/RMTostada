d18863ca546384169b33eb1ba00558d5de12005f
Rogelio Martinez Kobashi <rgkobashi@gmail.com>
2016-11-13 01:43:18 -0500

    Initial Commit

---
+++ RMTostada/RMTostada.h
@@ -0,0 +1,19 @@
+//
+//  RMTostada.h
+//  RMTostada
+//
+//  Created by Rogelio Martinez Kobashi on 11/13/16.
+//  Copyright © 2016 Rogelio Martinez Kobashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMTostada.
+FOUNDATION_EXPORT double RMTostadaVersionNumber;
+
+//! Project version string for RMTostada.
+FOUNDATION_EXPORT const unsigned char RMTostadaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMTostada/PublicHeader.h>
+
+
